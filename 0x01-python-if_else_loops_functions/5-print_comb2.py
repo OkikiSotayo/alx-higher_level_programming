a22728c2b2af84e06d44921e86fd7e1ba8d0470d
OkikiSotayo <okikisotayo@gmail.com>
2022-11-30 15:54:03 -0800

    combo error fixed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-         print(i)
-     else:
-         print("{}".format('0' + str(i) if i < 10 else i), end=", ")
+for number in range(100):
+    if (number != 99):
+        print("{}{}, ".format(int(number / 10), number % 10), end="")
+    else:
+        print("{}{}".format(int(number / 10), number % 10))
